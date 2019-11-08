deb4da2bdb98a1f73bcff417877c24172c62b9b7
Ray <r.samwise@gmail.com>
2019-11-08 10:06:41 +0400

    box

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main
 {
+    public static int truckCapacity;
+    public static int containerCapacity;
+    public static int boxCnt;
+    public static int containerCnt;
+    public static int truckCnt;
     public static void main(String[] args)
     {
         Container container = new Container();
@@ -18,6 +25,39 @@ public class Main
         System.out.println("Максимум Float " + Float.MAX_VALUE);
         System.out.println("Минимум Double " + Double.MIN_VALUE);
         System.out.println("Максимум Double " + Double.MAX_VALUE);
+        System.out.println("//////////////////////////////////////////////");
+        System.out.println("Задайте вместимость грузовика: ");
+        Scanner in1 = new Scanner(System.in);
+        truckCapacity = in1.nextInt();
+        System.out.println("Задайте вместимость контейнера: ");
+        Scanner in2 = new Scanner(System.in);
+        containerCapacity = in2.nextInt();
+        System.out.println("Введите необходимое количество ящиков: ");
+        Scanner in3 = new Scanner(System.in);
+        boxCnt = in3.nextInt();
+
+        System.out.println(getContCount(boxCnt));
+        System.out.println(getTruckCount(getContCount(boxCnt)));
+        int c = 1;  // containers
+        int t = 1;  //trucks
+        int i = 1; //boxes
+        int b = 1; //box_counter
+        for (t = 1; t <= getTruckCount(getContCount(boxCnt)) ; t++ )
+        {
+            System.out.println("Грузовик " + t);
+               for (c = 1; c <= getContCount(boxCnt); c++)
+               {
+                   System.out.println("  Контейнер " + c);
+                    for ( i = b; i <= boxCnt; i++)
+                    {
+                        System.out.println("        Ящик " + b);
+                        b++;
+                        if (i % containerCapacity == 0) break;
+                    }
+                   if (c % truckCapacity == 0) break;
+               }
+        }
+
 
 
     }
@@ -38,4 +78,20 @@ public class Main
 
     }
 
-}
+    public static int getContCount(int boxCnt)
+    {
+        if (boxCnt % containerCapacity > 0)
+        { containerCnt = (boxCnt / containerCapacity) + 1; }
+                   else {containerCnt = boxCnt / containerCapacity; }
+        return containerCnt;
+    }
+    public static int getTruckCount(int containerCnt)
+    {
+        if (containerCnt % truckCapacity > 0)
+        { truckCnt = (containerCnt / truckCapacity) + 1; }
+                   else {truckCnt = (containerCnt / truckCapacity); }
+        return truckCnt;
+    }
+
+    }
+
